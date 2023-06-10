b45c2e2e3c349519d5e8cd150b8b965fa8b4002a
Christian E <christianelena92@gmail.com>
2023-06-10 15:54:50 -0500

    Create Navbar with Mobile-Menu add state to hadle menu,ADD Contact Banner, Toolbar, PhoneIcon MUI, add styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,97 @@
 import React from "react";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { AppBar, Toolbar, IconButton, Menu, MenuItem } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import logo from "../img/logo-trans.svg";
+import "./Navbar.css";
+import { styled } from "@mui/system";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 export default function Navbar() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const [mobileMenuAnchorEl, setMobileMenuAnchorEl] = React.useState(null);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false); // Added state variable
 
   const handleMobileMenuOpen = (event) => {
     setMobileMenuAnchorEl(event.currentTarget);
+    setIsMenuOpen(true); // Update menu state
   };
 
   const handleMobileMenuClose = () => {
     setMobileMenuAnchorEl(null);
+    setIsMenuOpen(false); // Update menu state
   };
 
+  const ContactBanner = styled(Typography)`
+    padding: 8px;
+    background-color: #164801;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: 600;
+    color: white;
+  `;
+
+  const PhoneNumber = styled("span")`
+    margin: 0 5px;
+    font-weight: 600;
+  `;
+  const PhoneNumberLink = styled("a")`
+    text-decoration: none;
+    color: white;
+  `;
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <img src={logo} alt="Logo" className="logo" />
+    <AppBar position="static" color="transparent">
+      <ContactBanner>
+        CALL US TODAY!
+        <PhoneIcon />
+        <PhoneNumber>
+          <PhoneNumberLink href="tel:9493759322">(949)375-9322</PhoneNumberLink>
+        </PhoneNumber>
+      </ContactBanner>
+      <Toolbar
+        style={{
+          display: "flex",
+          flexDirection: isMobile ? "column" : "row",
+          alignItems: isMobile ? "flex-start" : "center",
+          justifyContent: isMobile ? "center" : "space-between",
+        }}
+      >
+        <div style={{ alignSelf: isMobile ? "center" : "flex-start" }}>
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
 
         {isMobile ? (
-          // Render hamburger menu on mobile
-          <>
+          <div style={{ width: "100%" }}>
             <IconButton
               size="large"
-              edge="start"
-              color="inherit"
+              edge="end"
               aria-label="menu"
               aria-haspopup="true"
               onClick={handleMobileMenuOpen}
+              sx={{
+                borderRadius: 0,
+                backgroundColor: "",
+                color: "#164801",
+                border: "solid #164801 0.1px",
+                "& svg": {
+                  fontSize: "2.565rem",
+                },
+              }}
             >
-              <MenuIcon />
+              {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
             </IconButton>
 
             <Menu
@@ -43,21 +100,21 @@ export default function Navbar() {
               keepMounted
               open={Boolean(mobileMenuAnchorEl)}
               onClose={handleMobileMenuClose}
+              classes={{ paper: "mobile-menu-paper" }}
             >
               <MenuItem onClick={handleMobileMenuClose}>Home</MenuItem>
               <MenuItem onClick={handleMobileMenuClose}>About</MenuItem>
               <MenuItem onClick={handleMobileMenuClose}>Services</MenuItem>
               <MenuItem onClick={handleMobileMenuClose}>Contact</MenuItem>
             </Menu>
-          </>
+          </div>
         ) : (
-          // Render app bar with responsive menu on medium and large screens
-          <>
+          <div style={{ display: "flex" }}>
             <MenuItem>Home</MenuItem>
             <MenuItem>About</MenuItem>
             <MenuItem>Services</MenuItem>
             <MenuItem>Contact</MenuItem>
-          </>
+          </div>
         )}
       </Toolbar>
     </AppBar>
