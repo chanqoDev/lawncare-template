d6bda1cb155b856f731ff35a6fd3cadb86697ec5
Christian E <christianelena92@gmail.com>
2023-06-20 01:57:57 -0500

    Update styles in About.js
     import the Box component from @mui/system and update the styling for the ABOUT US section. Add a Box component with textAlign set to center and marginTop of 4. Inside the Box, include a Typography component with variant set to h4, fontSize of 36px  marginBottom of 2, and color of #333. The Typography component displays the text ABOUT US.
    
    This commit enhances the styling of the ABOUT US section in the About.js file.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Typography, Container, Grid } from "@mui/material";
 import about from "../img/About.png";
 import ColumnStats from "./ColumnStats";
+import { Box } from "@mui/system";
 
 const AboutUsContainer = styled(Container)`
   background-color: #f5f5f5;
@@ -25,6 +26,14 @@ const Image = styled.img`
 const AboutUs = () => {
   return (
     <>
+      <Box sx={{ textAlign: "center", mt: 4 }}>
+        <Typography
+          variant="h4"
+          sx={{ fontSize: "36px", mb: 2, color: "#333" }}
+        >
+          ABOUT US
+        </Typography>
+      </Box>
       <AboutUsContainer>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
