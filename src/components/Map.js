d53478ab11c5340147bae8a4a5967012b05d385c
Christian E <christianelena92@gmail.com>
2023-06-19 21:34:02 -0500

    Feat: Add react-google-maps dependency and create Map component
    - Added the [react-google-maps] package using Yarn.
    - Created a configuration file for the Google Maps API (API keys removed for security).
    - Implemented a new component called [Map] using React.
    - Imported necessary components from [react-google-maps/api].
    - Imported the configuration file with the API key.
    - Defined container style, center coordinates, and map options for the Google Map.
    - Wrapped the component with [ LoadScript ] and passed the API key.
    - Created a 'GoogleMap' component with container style, center coordinates, zoom level, and map options.
    - Added a marker at the center position.
    - Exported the 'Map' component as the default.
    
    Note: Please make sure to add the API key back to the configuration file before using the Map component.

---
+++ src/components/Map.js
@@ -0,0 +1,251 @@
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import API_KEY from "./services/config";
+
+const Map = () => {
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+    justifySelf: "center",
+    alignSelf: "center",
+  };
+
+  const center = {
+    lat: 39.099724,
+    lng: -94.578331,
+  };
+
+  const mapOptions = {
+    styles: [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#ebe3cd",
+          },
+        ],
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#523735",
+          },
+        ],
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#f5f1e6",
+          },
+        ],
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#c9b2a6",
+          },
+        ],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#dcd2be",
+          },
+        ],
+      },
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#ae9e90",
+          },
+        ],
+      },
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#93817c",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#a5b076",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#447530",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#f5f1e6",
+          },
+        ],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#fdfcf8",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#f8c967",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#e9bc62",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#e98d58",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#db8555",
+          },
+        ],
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#806b63",
+          },
+        ],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae",
+          },
+        ],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#8f7d77",
+          },
+        ],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#ebe3cd",
+          },
+        ],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#dfd2ae",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#b9d3c2",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#92998d",
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={API_KEY}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        options={mapOptions}
+      >
+        {/* Add your markers here */}
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Map;
